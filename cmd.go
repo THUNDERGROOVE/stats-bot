151c23576348d2af68ddd20b1a7701d3d529a2f0
Nick Powell <markusthefish@gmail.com>
2015-07-22 15:34:05 -0700

    Server names now

--- cmd.go
+++ cmd.go
@@ -13,10 +13,10 @@ var Commands = make(map[string]*Cmd)
 
 const lookup = `
 {{if .Character}}
-({{.Faction.Name.En}}) {{if .Outfit.Alias }}[{{.Outfit.Alias}}]{{end}} {{.Name.First}} BR: {{.Battlerank.Rank}} :cert: {{.GetCerts}}\
+({{.Faction.Name.En}}) {{if .Outfit.Alias }}[{{.Outfit.Alias}}]{{end}} {{.Name.First}}@{{.ServerName}} BR: {{.Battlerank.Rank}} :cert: {{.GetCerts}}\
 Kills: {{.GetKills}} Deaths: {{.GetDeaths}} KDR: {{.KDR}}\
 {{if .Outfit.Name}} Outfit: {{.Outfit.Name}} with {{.Outfit.MemberCount}} members {{end}}\
-Defended: {{.GetFacilitiesDefended}} Captured: {{.GetFacilitiesCaptured}}\
+Defended: {{.GetFacilitiesDefended}} Captured: {{.GetFacilitiesCaptured}}
 {{else}}
 Uh got nil character?
 {{end}}
