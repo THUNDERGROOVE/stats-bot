07bfa503f2a59cac82f53217f37edc774b58a6ed
Nick Powell <markusthefish@gmail.com>
2015-08-17 23:23:09 -0700

    !report now

--- blacklist.go
+++ blacklist.go
@@ -3,11 +3,11 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/THUNDERGROOVE/stats-bot/db"
 	"log"
+	"strings"
 	"text/template"
 
-	"strings"
+	"github.com/THUNDERGROOVE/stats-bot/db"
 )
 
 var searchTmpl *template.Template
@@ -17,7 +17,41 @@ func init() {
 	searchTmpl = parseTemplate("search.tmpl")
 
 	RegisterCommand("reportpsn", cmdReportPSN, CMD_DEV)
+	RegisterCommand("report", cmdReport, CMD_DEV)
 	RegisterCommand("searchreport", cmdSearchReports, CMD_DEV)
+	RegisterCommand("isadmin", cmdIsAdmin, CMD_READY)
+}
+
+func cmdReport(ctx *Context) {
+	args := strings.Split(ctx.Ev.Text, " ")
+	if len(args) < 2 {
+		ctx.Respond("report <name> <additional information>")
+	}
+	name := args[1]
+
+	info := strings.Join(args[len(args)-2:], " ")
+
+	// Lookup Character to know what region they play on
+
+	// TODO: Refactor this with PSN version
+	char, err := Census.GetCharacterByName(name)
+
+	if err != nil {
+		err = nil
+		char, err = CensusEU.GetCharacterByName(name)
+		if err != nil {
+			ctx.Respond("That name didn't exist in either US or EU")
+			return
+		}
+	} else {
+		_, err := CensusEU.GetCharacterByName(name)
+		if err == nil {
+			ctx.Respond("The given character name matches on US and EU")
+		}
+	}
+	db.NewReport(ctx.Ev.Name, char.Name.First, "not specified", info, char.Parent)
+	ctx.Respond(fmt.Sprintf("Reported: %v for %v.", char.Name.First, info))
+
 }
 
 func cmdSearchReports(ctx *Context) {
@@ -29,6 +63,7 @@ func cmdSearchReports(ctx *Context) {
 
 	db.DB.Where("name like ?", fmt.Sprintf("%%%v%%", search)).Find(&reports)
 	g["Reports"] = reports
+
 	buf := bytes.NewBufferString("")
 	searchTmpl.Execute(buf, g)
 	ctx.Respond(buf.String())
